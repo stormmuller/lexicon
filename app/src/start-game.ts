af7f4019627b5ddd42a08a6eaea7103aa6451410
Storm <stormmuller2@gmail.com>
2024-12-18 10:19:38 +0200

    Fixed a bug where the game would start twice

--- app/src/start-game.ts
+++ app/src/start-game.ts
@@ -1,13 +1,24 @@
 import { game } from "@gameup/engine";
 import { createMainScene } from "./scenes";
 
+let gameStarted = false;
+
 export function startGame() {
   window.setTimeout(async () => {
+    if (gameStarted) {
+      console.warn("[Lexicon] game already started, skipping initialization!");
+      return; // Do not start the game twice
+    }
+
+    console.log("[Lexicon] initializing...");
+
+    gameStarted = true;
+    
     const wordTreeGame = new game.Game();
     const mainScene = await createMainScene(wordTreeGame);
-
+    
     wordTreeGame.registerScene(mainScene);
-
+    
     wordTreeGame.run();
   }, 100); // TODO: hack for now because I don't have time to deal with the window resizing...
 }
